25ce5247b0ccd4b81a33ddfbabeb214469f968ba
hungry1ion <160570451+hungry1ion@users.noreply.github.com>
2024-07-12 01:25:55 +0900

    Add files via upload
    10개단위 ->100개 단위 판매 수정
    아비도스 재료들 추가(고고학 낚시)
    다른 생활 추후 추가 예정

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ window.onload = function () {
 
     Promise.all([
         fetchItemPrices(url, authorizationToken, 50010, "상급 오레하 융화 재료", ['resultPrice1', 'resultPrice2']),
-        fetchItemPrices(url, authorizationToken, 90700, "유물", ['materialPrice1', 'materialPrice2', 'materialPrice3']),
-        fetchItemPrices(url, authorizationToken, 90600, "생선", ['materialPrice4']),
-        fetchItemPrices(url, authorizationToken, 90600, "자연산 진주", ['materialPrice5']),
-        fetchItemPrices(url, authorizationToken, 90600, "오레하 태양 잉어", ['materialPrice6'])
+        fetchItemPrices(url, authorizationToken, 50010, "아비도스 융화 재료", ['resultPrice3']),
+        fetchItemPrices(url, authorizationToken, 90700, "고대 유물", ['ancientArtifactPrice']),
+        fetchItemPrices(url, authorizationToken, 90700, "희귀한 유물", ['rareArtifactPrice']),
+        fetchItemPrices(url, authorizationToken, 90700, "오레하 유물", ['orehaArtifactPrice']),
+        fetchItemPrices(url, authorizationToken, 90700, "아비도스 유물", ['abydosArtifactPrice']),
+        fetchItemPrices(url, authorizationToken, 90600, "생선", ['fishPrice']),
+        fetchItemPrices(url, authorizationToken, 90600, "붉은 살 생선", ['redFishPrice']),
+        fetchItemPrices(url, authorizationToken, 90600, "오레하 태양 잉어", ['orehaFishPrice']),
+        fetchItemPrices(url, authorizationToken, 90600, "아비도스 태양 잉어", ['abydosFishPrice'])
     ]).then(() => {
         displayProfit();
     }).catch(error => {
@@ -20,15 +25,19 @@ function changeMaterialPrice(materialInputId, materialPriceId) {
     document.getElementById(materialPriceId).textContent = isNaN(inputVal) ? "0" : inputVal;
 }
 
-document.getElementById('material1').oninput = function () { changeMaterialPrice('material1', 'materialPrice1'); displayProfit(); };
-document.getElementById('material2').oninput = function () { changeMaterialPrice('material2', 'materialPrice2'); displayProfit(); };
-document.getElementById('material3').oninput = function () { changeMaterialPrice('material3', 'materialPrice3'); displayProfit(); };
-document.getElementById('material4').oninput = function () { changeMaterialPrice('material4', 'materialPrice4'); displayProfit(); };
-document.getElementById('material5').oninput = function () { changeMaterialPrice('material5', 'materialPrice5'); displayProfit(); };
-document.getElementById('material6').oninput = function () { changeMaterialPrice('material6', 'materialPrice6'); displayProfit(); };
+document.getElementById('ancientArtifact').oninput = function () { changeMaterialPrice('ancientArtifact', 'ancientArtifactPrice'); displayProfit(); };
+document.getElementById('rareArtifact').oninput = function () { changeMaterialPrice('rareArtifact', 'rareArtifactPrice'); displayProfit(); };
+document.getElementById('orehaArtifact').oninput = function () { changeMaterialPrice('orehaArtifact', 'orehaArtifactPrice'); displayProfit(); };
+document.getElementById('abydosArtifact').oninput = function () { changeMaterialPrice('abydosArtifact', 'abydosArtifactPrice'); displayProfit(); };
+
+document.getElementById('fish').oninput = function () { changeMaterialPrice('fish', 'fishPrice'); displayProfit(); };
+document.getElementById('redFish').oninput = function () { changeMaterialPrice('redFish', 'redFishPrice'); displayProfit(); };
+document.getElementById('orehaFish').oninput = function () { changeMaterialPrice('orehaFish', 'orehaFishPrice'); displayProfit(); };
+document.getElementById('abydosFish').oninput = function () { changeMaterialPrice('abydosFish', 'abydosFishPrice'); displayProfit(); };
 
 document.getElementById('result1').oninput = function () { changeMaterialPrice('result1', 'resultPrice1'); displayProfit(); };
 document.getElementById('result2').oninput = function () { changeMaterialPrice('result2', 'resultPrice2'); displayProfit(); };
+document.getElementById('result3').oninput = function () { changeMaterialPrice('result3', 'resultPrice3'); displayProfit(); };
 
 document.getElementById('reducedFee').oninput = function () { changeMaterialPrice('reducedFee', 'reducedFeeAmount'); displayProfit(); };
 document.getElementById('increasedGreat').oninput = function () { changeMaterialPrice('increasedGreat', 'increasedGreatAmount'); displayProfit(); };
@@ -64,20 +73,30 @@ function calculateProfit(materialPrices, resultPriceId, goldUsed, materialRates,
 }
 
 function displayProfit() {
-    var bundle1 = calculateProfit(['materialPrice1', 'materialPrice2', 'materialPrice3'], 'resultPrice1', 250, [0.94, 2.9, 1.6], 20);
+    var bundle1 = calculateProfit(['ancientArtifactPrice', 'rareArtifactPrice', 'orehaArtifactPrice'], 'resultPrice1', 250, [0.94, 0.29, 0.16], 20);
     document.getElementById('bundleProfit1').innerHTML = bundle1.totalProfit;
     document.getElementById('singleProfit1').innerHTML = bundle1.singleProfit;
     document.getElementById('useProfit1').innerHTML = bundle1.useProfit;
 
-    var bundle2 = calculateProfit(['materialPrice1', 'materialPrice2', 'materialPrice3'], 'resultPrice2', 300, [1.07, 5.1, 5.2], 15);
+    var bundle2 = calculateProfit(['ancientArtifactPrice', 'rareArtifactPrice', 'orehaArtifactPrice'], 'resultPrice2', 300, [1.07, 0.51, 0.52], 15);
     document.getElementById('bundleProfit2').innerHTML = bundle2.totalProfit;
     document.getElementById('singleProfit2').innerHTML = bundle2.singleProfit;
     document.getElementById('useProfit2').innerHTML = bundle2.useProfit;
 
-    var bundle3 = calculateProfit(['materialPrice4', 'materialPrice5', 'materialPrice6'], 'resultPrice2', 300, [1.42, 6.9, 5.2], 15);
+    var bundle3 = calculateProfit(['fishPrice', 'redFishPrice', 'orehaFishPrice'], 'resultPrice2', 300, [1.42, 0.69, 0.52], 15);
     document.getElementById('bundleProfit3').innerHTML = bundle3.totalProfit;
     document.getElementById('singleProfit3').innerHTML = bundle3.singleProfit;
     document.getElementById('useProfit3').innerHTML = bundle3.useProfit;
+
+    var bundle4 = calculateProfit(['ancientArtifactPrice', 'rareArtifactPrice', 'abydosArtifactPrice'], 'resultPrice3', 364, [0.86, 0.45, 0.33], 10);
+    document.getElementById('bundleProfit4').innerHTML = bundle4.totalProfit;
+    document.getElementById('singleProfit4').innerHTML = bundle4.singleProfit;
+    document.getElementById('useProfit4').innerHTML = bundle4.useProfit;
+
+    var bundle5 = calculateProfit(['fishPrice', 'redFishPrice', 'abydosFishPrice'], 'resultPrice3', 364, [0.86, 0.45, 0.33], 10);
+    document.getElementById('bundleProfit5').innerHTML = bundle5.totalProfit;
+    document.getElementById('singleProfit5').innerHTML = bundle5.singleProfit;
+    document.getElementById('useProfit5').innerHTML = bundle5.useProfit;
 
 }
 
@@ -138,13 +157,20 @@ document.getElementById('fetchPriceButton').addEventListener('click', function (
 
     Promise.all([
         fetchItemPrices(url, authorizationToken, 50010, "상급 오레하 융화 재료", ['resultPrice1', 'resultPrice2']),
-        fetchItemPrices(url, authorizationToken, 90700, "유물", ['materialPrice1', 'materialPrice2', 'materialPrice3']),
-        fetchItemPrices(url, authorizationToken, 90600, "생선", ['materialPrice4']),
-        fetchItemPrices(url, authorizationToken, 90600, "자연산 진주", ['materialPrice5']),
-        fetchItemPrices(url, authorizationToken, 90600, "오레하 태양 잉어", ['materialPrice6'])
+        fetchItemPrices(url, authorizationToken, 50010, "아비도스 융화 재료", ['resultPrice3']),
+        fetchItemPrices(url, authorizationToken, 90700, "고대 유물", ['ancientArtifactPrice']),
+        fetchItemPrices(url, authorizationToken, 90700, "희귀한 유물", ['rareArtifactPrice']),
+        fetchItemPrices(url, authorizationToken, 90700, "오레하 유물", ['orehaArtifactPrice']),
+        fetchItemPrices(url, authorizationToken, 90700, "아비도스 유물", ['abydosArtifactPrice']),
+        fetchItemPrices(url, authorizationToken, 90600, "생선", ['fishPrice']),
+        fetchItemPrices(url, authorizationToken, 90600, "붉은 살 생선", ['redFishPrice']),
+        fetchItemPrices(url, authorizationToken, 90600, "오레하 태양 잉어", ['orehaFishPrice']),
+        fetchItemPrices(url, authorizationToken, 90600, "아비도스 태양 잉어", ['abydosFishPrice'])
     ]).then(() => {
         displayProfit();
+
     }).catch(error => {
         console.error('Error fetching item prices:', error);
     });
+
 });
